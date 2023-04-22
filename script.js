6054028959d9118f5ed47683d0fac7d848c57909
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2023-04-22 13:31:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// const subsection = document.querySelectorAll(".subsection");
+// const calender = document.querySelector(".calender");
+
+//  function hideshow() {
+//     for (let i=0; i <= subsection.length; i++){ 
+//         if (subsection[i].style.display === "none"){
+//             subsection[i].style.display = "block";
+//         } else {
+//             subsection[i].style.display = "none";
+//         }
+//      }
+//  };
+
+// hideshow();
+
+// const weeks = document.querySelectorAll(".week");
+
+
+//  function hideshowtwo() {
+//     let value = 1;
+//     for (let i=0; i <= weeks.length; i++){ 
+//         if (value === 0){
+//             const divy = document.createElement("div");
+//             divy.classList.add("divy");
+//             calender.appendChild("divy");
+//             value = 1;
+//         } else {
+//             divy.classList.remove("divy");
+//             calender.removeChild("divy");
+//             value = 0;
+//         }
+//      }
+//  };
