cc5bd0695f16e78462fc52c1233a02c9c91a889a
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2023-04-23 20:58:00 +0300

    yes

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-// const subsection = document.querySelectorAll(".subsection");
-// const calender = document.querySelector(".calender");
-
-//  function hideshow() {
-//     for (let i=0; i <= subsection.length; i++){ 
-//         if (subsection[i].style.display === "none"){
-//             subsection[i].style.display = "block";
-//         } else {
-//             subsection[i].style.display = "none";
-//         }
-//      }
-//  };
-
-// hideshow();
-
-// const weeks = document.querySelectorAll(".week");
+const subsection = document.querySelectorAll(".subsection");
+const section = document.querySelectorAll(".section");
+const colapsable = document.querySelectorAll(".colapsable");
 
 
-//  function hideshowtwo() {
-//     let value = 1;
-//     for (let i=0; i <= weeks.length; i++){ 
-//         if (value === 0){
-//             const divy = document.createElement("div");
-//             divy.classList.add("divy");
-//             calender.appendChild("divy");
-//             value = 1;
-//         } else {
-//             divy.classList.remove("divy");
-//             calender.removeChild("divy");
-//             value = 0;
-//         }
-//      }
-//  };
+ 
+    let i = 0;
+    for (let i=0; i <= colapsable.length; i++){ 
+        colapsable[i].addEventListener("click", function {
+            const content  = this.nextElementSibling;
+            if (content.style.display == "block"){
+                            content.style.display = "none";
+                        } else {
+                            content.style.display = "block";
+                        }})}
