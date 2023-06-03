9d69e4528c3f54d811c498bed6bc64d2f53d3dcf
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2023-06-03 19:57:37 +0300

    two eventlistner button

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //Selectors
-const subsection = document.querySelector(".subsection");
-const subsection2 = document.querySelector(".subsection2");
+const subsection = document.querySelectorAll(".subsection");
 const subsection3 = document.querySelector(".subsection3");
 const subsection4 = document.querySelector(".subsection4");
 const subsection5 = document.querySelector(".subsection5");
@@ -12,31 +11,22 @@ const arrow5 = document.querySelector(".chevron-down-sharp5");
 
 
 //Event Listener
-arrow.addEventListener("click", Collapse)
-arrow2.addEventListener("click", Collapse2)
+arrow.addEventListener("click", Collapse);
 arrow3.addEventListener("click", Collapse3)
 arrow4.addEventListener("click", Collapse4)
 arrow5.addEventListener("click", Collapse5)
 
 
-//functions      
+//functions 
 function Collapse() {
-    if (subsection.style.display === "none") {
-        subsection.style.display = "block";
+    for (let i = 0; i <= subsection.length; i++) {
+        if (subsection[i].style.display === "none") {
+            subsection[i].style.display = "block";
+        }
+        else {
+            subsection[i].style.display = "none";
+        }
     }
-    else {
-        subsection.style.display = "none";
-    }
-
-};
-function Collapse2() {
-    if (subsection2.style.display === "none") {
-        subsection2.style.display = "block";
-    }
-    else {
-        subsection2.style.display = "none";
-    }
-
 };
 function Collapse3() {
     if (subsection3.style.display === "none") {
