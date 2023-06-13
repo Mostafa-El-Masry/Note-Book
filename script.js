c783687904ea79ac0647e6adffc617d6254ff891
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2023-06-13 22:05:57 +0300

    adding passwordia section adding li adding check box

--- script.js
+++ script.js
@@ -3,13 +3,35 @@ const subsections = document.querySelectorAll(".subsection");
 const buttons = document.querySelectorAll(".chevron-down-sharp");
 const weeks = document.querySelectorAll(".week");
 const months = document.querySelectorAll(".month");
+const submit = document.querySelector("#button-addon2");
+const input = document.querySelector(".form-control");
+const plist = document.querySelector(".p-list");
+// //Event Listener
+// for (subsection of subsections) {
+//     subsection.style.display = "none";
+// }
 
-
-
-//Event Listener
-for (subsection of subsections) {
-    subsection.style.display = "none";
+submit.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (input.value !== ""){
+    const pdiv = document.createElement("div");
+    pdiv.classList.add("Password");
+    const pli1 = document.createElement("li");
+    pli1.innerText = input.value;
+    const pli2 = document.createElement("i")
+    pli2.classList.add("fa-solid", "fa-check");
+    pli1.appendChild(pli2);
+    pdiv.appendChild(pli1);
+    plist.appendChild(pdiv);
+    input.value = "";
 }
+});
+
+
+
+
+
+
 
 //loops
 
@@ -48,7 +70,7 @@ for (x of months) {
         }
     });
 }
-
+ // Functions 
 
 
 //local storage
