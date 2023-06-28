17259f02170e4b1dabe425069d3e4edf79fc2aeb
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2023-06-28 16:49:34 +0300

    local storage first att

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 //Selectors
 const subsections = document.querySelectorAll(".subsection");
-const buttons = document.querySelectorAll(".chevron-down-sharp");
+const buttons = document.querySelectorAll(".fa-chevron-down");
 const weeks = document.querySelectorAll(".week");
 const months = document.querySelectorAll(".month");
 const submit = document.querySelector("#button-addon2");
 const input = document.querySelector(".form-control");
+const plist = document.querySelector(".p-list");
 
 // //Event Listener
 // for (subsection of subsections) {
 //     subsection.style.display = "none";
 // }
-
+document.addEventListener("DOMContentLoaded", getpasswords);
 submit.addEventListener('click', function (event) {
     event.preventDefault();
-    // gabing ul 
-    const plist = document.querySelector(".p-list");
-    
+
     if (input.value !== ""){
     //creating div and appending it to ul
     const pdiv = document.createElement("div");
@@ -28,14 +27,22 @@ submit.addEventListener('click', function (event) {
     pdiv.appendChild(pli);
     pli.innerText = input.value;
    
+    //Save to local - do this last
+    //Save to local
+    saveLocalpasswords(input.value);
     // creating a button and apending it to div 
     const deleteBut = document.createElement("button");
     deleteBut.classList.add("delete-But");
     deleteBut.innerHTML = '<i class="fa-solid fa-square-minus"></i>';
     pdiv.appendChild(deleteBut);
 
-    deleteBut.addEventListener('click', function (){
-        plist.removeChild(pdiv);
+    deleteBut.addEventListener('click', function deleteButton (e){
+        e.stopPropagation();
+        const item = e.target.parentElement;
+        item.classList.add("fall")
+        item.addEventListener('transitionend', function () {
+            item.remove();
+        })
     });
 
     // clearing input value
@@ -92,3 +99,60 @@ for (x of months) {
 
 
 //local storage
+function saveLocalpasswords(pdiv) {
+    let passwords;
+    if (localStorage.getItem("pdiv") === null) {
+      passwords = [];
+    } else {
+      passwords = JSON.parse(localStorage.getItem("passwords"));
+    }
+    passwords.push(pdiv);
+    localStorage.setItem("passwords", JSON.stringify(passwords));
+  }
+  function removeLocalpasswords(pdiv) {
+    let passwords;
+    if (localStorage.getItem("passwords") === null) {
+      passwords = [];
+    } else {
+      passwords = JSON.parse(localStorage.getItem("passwords"));
+    }
+    const pdivIndex = pdiv.children[0].innerText;
+    passwords.splice(passwords.indexOf( pdivIndex), 1);
+    localStorage.setItem("passwords", JSON.stringify(passwords));
+  }
+  
+  function getpasswords() {
+    let passwords;
+
+    if (localStorage.getItem("passwords") === null) {
+      passwords = [];
+    } else {
+      passwords = JSON.parse(localStorage.getItem("passwords"));
+    }
+    passwords.forEach(function(pdiv) {
+        if (input.value !== ""){
+        //creating div and appending it to ul
+        const pdiv = document.createElement("div");
+        plist.appendChild(pdiv);
+        //adding class to it
+        pdiv.classList.add("Password");
+        // creating li and appending it to div
+        const pli = document.createElement("li");
+        pli.classList.add("p-li");
+        pdiv.appendChild(pli);
+        pli.innerText = pdiv;
+        //Create list
+        
+      
+      // creating a button and apending it to div 
+    const deleteBut = document.createElement("button");
+    deleteBut.classList.add("delete-But");
+    deleteBut.innerHTML = '<i class="fa-solid fa-square-minus"></i>';
+    pdiv.appendChild(deleteBut);
+
+      //attach final Todo
+      plist.appendChild(pdiv);
+    }
+      
+    });
+  }
