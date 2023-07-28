b33ac3fc6b48167920f7d1697e4c967f52e02f42
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2023-07-29 00:02:01 +0300

    updating weekly calender using chat gpt4

--- script.js
+++ script.js
@@ -27,3 +27,57 @@ for (const i of buttons) {
       }
   });
 }
+
+
+const collapsibles = document.getElementsByClassName("weekcollapsible");
+const goalContainer = document.getElementById("goalContainer");
+const goalText = document.getElementById("goalText");
+const saveGoal = document.getElementById("saveGoal");
+const calendar = document.getElementById("calendar");
+const saved = document.getElementsByClassName("saved");
+let selectedWeek = null;
+
+function createCalendar() {
+  const startDate = new Date(2023, 0, 1);
+  while (startDate.getDay() !== 0) {
+    startDate.setDate(startDate.getDate() - 1);
+  }
+  let weekCount = 1;
+  while (startDate.getFullYear() <= 2023) {
+    const weekDiv = document.createElement("div");
+    weekDiv.textContent = `Week ${weekCount++}: ${startDate.toDateString()}`;
+    weekDiv.style.cursor = "pointer";
+    weekDiv.onclick = function () {
+      selectedWeek = this.textContent;
+      goalText.value = localStorage.getItem(selectedWeek) || "";
+      goalContainer.style.display = "block";
+     
+    };
+    calendar.appendChild(weekDiv);
+    weekDiv.classList.add("style");
+    saved.innerText = "Goal saved!";
+    saved.innerHTML = "Goal saved!";
+    startDate.setDate(startDate.getDate() + 7);
+  }
+}
+
+for (let i = 0; i < collapsibles.length; i++) {
+  collapsibles[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    const content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+saveGoal.addEventListener("click", function () {
+  if (selectedWeek) {
+    localStorage.setItem(selectedWeek, goalText.value);
+    alert("Goal saved!");
+  }
+});
+
+createCalendar();
